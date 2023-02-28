6dfa68dc01e9d39d63f35cc713fbb92f14be2df7
Ueslei Lima <ueslei@backbase.com>
2023-02-28 20:35:10 +0100

    Making balance requests in parallel.

--- src/main/java/com/backbase/mastercard/service/BalanceService.java
+++ src/main/java/com/backbase/mastercard/service/BalanceService.java
@@ -1,5 +1,7 @@
 package com.backbase.mastercard.service;
 
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
 import com.backbase.arrangement.integration.outbound.balance.models.BalanceItem;
 import com.backbase.buildingblocks.presentation.errors.BadRequestException;
 import com.backbase.mastercard.mapping.BalanceMapper;
@@ -12,9 +14,14 @@ import com.mastercard.openbanking.accounts.models.PostAccountsAccountBalancesPar
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class BalanceService {
@@ -23,17 +30,21 @@ public class BalanceService {
 
     private final BalanceMapper balanceMapper;
 
+    private final Executor executor;
+
     public List<BalanceItem> getBalances(Set<String> arrangementIds) {
         return arrangementIds.stream()
             .map(id -> new PostAccountsAccountBalancesParamsBody()
                 .accountId(id)
                 .requestInfo(buildRequestInfo()))
-            .map(this::getArrangementBalance)
+            .map(balanceRequest -> supplyAsync(() -> getArrangementBalance(balanceRequest), executor))
+            .collect(Collectors.collectingAndThen(Collectors.toList(), c -> c.stream().map(CompletableFuture::join)))
             .toList();
     }
 
     private BalanceItem getArrangementBalance(PostAccountsAccountBalancesParamsBody balances) {
         try {
+            log.info("Fetching balances for account: {}", balances.getAccountId());
             PostAccountsAccountBalancesOKBody accountBalances = accountBalancesApi.getAccountBalances(balances);
             return balanceMapper.map(accountBalances);
         } catch (ApiException e) {
@@ -44,8 +55,8 @@ public class BalanceService {
     private PostAccountsAccountBalancesParamsBodyRequestInfo buildRequestInfo() {
         return new PostAccountsAccountBalancesParamsBodyRequestInfo()
             .xRequestId(UUID.randomUUID().toString())
-            .aspspId("420e5cff-0e2a-4156-991a-f6eeef0478cf")
             .consentId("GFiTpF3:EBy5xGqQMatk")
+            .aspspId("420e5cff-0e2a-4156-991a-f6eeef0478cf")
             .psuTppCustomerId("420e5cff-0e2a-4156-991a-f6eeef0478cf")
             .psuIPAddress("192.168.0.1")
             .psuAgent("PostmanRuntime/7.20.1")
