9d7eef7c1af85fb668d9681a7f27dfd08a660266
Ueslei Lima <ueslei@backbase.com>
2023-03-07 11:59:03 +0100

    Enabling actuator http tracing to support development.

--- src/main/java/com/backbase/mastercard/MastercardIntegrationApplication.java
+++ src/main/java/com/backbase/mastercard/MastercardIntegrationApplication.java
@@ -1,9 +1,15 @@
 package com.backbase.mastercard;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,6 +17,14 @@ public class MastercardIntegrationApplication extends SpringBootServletInitializ
 
     public static void main(final String[] args) {
         SpringApplication.run(MastercardIntegrationApplication.class, args);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConfigurationProperties(prefix = "management.trace.http.in-memory")
+    @ConditionalOnExpression("#{${management.endpoints.enabled-by-default:false} or ${management.trace.http.enabled:false}}")
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
     }
 
 }
